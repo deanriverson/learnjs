1dcb8763f1251e7162bdc5a2a11de1860a2a9c42
Dean Iverson <dean@pleasingsoftware.com>
2016-12-09 20:38:42 -0700

    Chapter 3 changes: Data model, events, styling, nav bar, app shell, etc.

---
+++ public/app.js
@@ -0,0 +1,113 @@
+"use strict";
+
+var learnjs = {};
+
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() { return __; }"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() { return 42 === 6 * __; }"
+    }
+];
+
+learnjs.appOnReady = function () {
+    window.onhashchange = function () {
+        learnjs.showView(window.location.hash);
+    };
+    learnjs.showView(window.location.hash);
+};
+
+learnjs.template = function (name) {
+    return $('.templates .' + name).clone();
+};
+
+learnjs.applyObject = function (obj, elem) {
+    for (var key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+
+learnjs.triggerEvent = function (name, args) {
+    $('.view-container > *').trigger(name, args);
+};
+
+learnjs.buildCorrectFlash = function (problemNumber) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (problemNumber < learnjs.problems.length) {
+        link.attr('href', '#problem-' + (problemNumber + 1));
+    } else {
+        link.attr('href', '');
+        link.text("You're Finished!");
+    }
+    return correctFlash;
+};
+
+learnjs.flashElement = function (elem, content) {
+    elem.fadeOut('fast', function () {
+        elem.html(content);
+        elem.fadeIn();
+    });
+};
+
+learnjs.problemView = function (data) {
+    var problemNumber = parseInt(data, 10);
+    var view = learnjs.template('problem-view');
+
+    var problemData = learnjs.problems[problemNumber - 1];
+    var resultFlash = view.find('.result');
+
+    function checkAnswer() {
+        var answer = view.find('.answer').val();
+        var test = problemData.code.replace('__', answer) + '; problem();';
+        return eval(test);
+    }
+
+    function checkAnswerClick() {
+        if (checkAnswer()) {
+            var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+            learnjs.flashElement(resultFlash, correctFlash);
+        } else {
+            learnjs.flashElement(resultFlash, 'Incorrect!');
+        }
+        return false;
+    }
+
+    if (problemNumber < learnjs.problems.length) {
+        var buttonItem = learnjs.template('skip-btn');
+        buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function () {
+            buttonItem.remove();
+        });
+    }
+
+    view.find('.check-btn').click(checkAnswerClick);
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(problemData, view);
+    return view;
+};
+
+learnjs.landingView = function () {
+    return learnjs.template('landing-view');
+};
+
+learnjs.showView = function (hash) {
+    var routes = {
+        '': learnjs.landingView,
+        '#': learnjs.landingView,
+        '#problem': learnjs.problemView
+    };
+
+    var path = hash.split('-');
+    var viewFn = routes[path[0]];
+    var viewParams = path.slice(1);
+
+    if (viewFn) {
+        learnjs.triggerEvent('removingView', []);
+        $('.view-container').empty().append(viewFn.apply(learnjs, viewParams));
+    }
+};
---
+++ public/tests/app_spec.js
@@ -0,0 +1,74 @@
+describe('LearnJS', function () {
+    it('can show a problem view', function () {
+        learnjs.showView('#problem-1');
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+
+    it('shows the landing page view when there is no hash', function () {
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it('passes the hash view parameter to the view function', function () {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    });
+
+    it('invokes the router when loaded', function () {
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('subscribes to the hash change event', function () {
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    describe('problem view', function () {
+        var view;
+        beforeEach(function() {
+            view = learnjs.problemView('1');
+        });
+
+        it('has a title that includes the problem number', function () {
+            expect(view.find('.title').text()).toEqual('Problem #1');
+        });
+
+        describe('answer section', function() {
+            it('can check a correct answer by hitting a button', function() {
+                view.find('.answer').val('true');
+                view.find('.check-btn').click();
+                expect(view.find('.result span').text()).toEqual('Correct!');
+            });
+
+            it('rejects an incorrect answer', function() {
+                view.find('.answer').val('false');
+                view.find('.check-btn').click();
+                expect(view.find('.result').text()).toEqual('Incorrect!');
+            });
+        });
+    });
+
+    describe('apply object', function () {
+        it('can apply a single object field to markup', function () {
+            var elem = $('<div><div data-name="test"></div></div>');
+            learnjs.applyObject({test: 'Testing'}, elem);
+            expect(elem.find('div[data-name]').text()).toEqual('Testing');
+        });
+
+        it('can apply all object fields to markup', function () {
+            var obj = {desc: 'Description', code: "if true"};
+            var elem = $('<div><div data-name="desc"></div><div data-name="code"></div></div>');
+
+            learnjs.applyObject(obj, elem);
+            var els = elem.find('div[data-name]');
+
+            expect(els[0].textContent).toEqual(obj.desc);
+            expect(els[1].textContent).toEqual(obj.code);
+        });
+    });
+});
